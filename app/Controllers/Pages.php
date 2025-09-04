883f526f6911cb5f95dc1f1c74ad954f9cbe2d7b
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-04 16:09:15 +0700

    views template selesai

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -5,19 +5,38 @@ namespace App\Controllers;
 class Pages extends BaseController
 {
     public function index()
-    {   
+    {
         $data = [
             "title" => "Home | Yossi Nordiansah"
         ];
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
 
     public function about()
-    {   
-        echo view('layout/header');
-        echo view('pages/about');
-        echo view('layout/footer');
+    {
+        $data = [
+            "title" => "About | Yossi Nordiansah"
+        ];
+        return view('pages/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            "title" => "Contact | Yossi Nordiansah",
+            "alamat" => [
+                [
+                    'tipe' => "rumah",
+                    "alamat" => "Jl Salak, RT 06, Rw 01, Segunung, Dlanggu",
+                    "kota" => "Mojokerto"
+                ],
+                [
+                    'tipe' => "Kantor",
+                    "alamat" => "Jl Jabon, RT 06, Rw 01, Jabon, Mojoanyar",
+                    "kota" => "Mojokerto"
+                ]
+                ]
+        ];
+        return view('pages/contact', $data);
     }
 }
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-
-<body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary container">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
@@ -24,7 +13,7 @@
                         <a class="nav-link" href="/about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="/contact">Contact</a>
                     </li>
                 </ul>
             </div>
