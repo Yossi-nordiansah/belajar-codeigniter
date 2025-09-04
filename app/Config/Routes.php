7862087ea585d59e2305ab24354c7a2bddb3dfcb
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-04 21:34:10 +0700

    model 1 selesai

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Pages::index');
 $routes->get('/about', 'Pages::about');
 $routes->get('/contact', 'Pages::contact');
+$routes->get('/komik', 'Komik::index');
