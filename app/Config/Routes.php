02278f7542dd555617621a4df0ec9b171ce858ca
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-04 14:10:16 +0700

    views selesai

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */ 
-$routes->get('/', 'Home::index');
-$routes->get('/about/(:any)/(:num)', 'About::index/$1/$2');
-$routes->get("/users", "Admin\Users::index");
+$routes->get('/', 'Pages::index');
+$routes->get('/about', 'Pages::about');
---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+   <div class="container">
+       <div class="row">
+           <div class="col">
+               <h1>About Me</h1>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, inventore cumque? Quaerat temporibus aliquid fugit fugiat maiores suscipit laborum ea modi ab assumenda molestiae quisquam praesentium, voluptatem vitae quos incidunt quasi reiciendis aut neque facilis deleniti! Excepturi officiis facilis corrupti ad dolore non commodi nam numquam laborum ducimus est minima sit minus et consectetur, hic illum veniam harum vero, deleniti, optio atque. Voluptatum natus velit porro facilis ipsam. Quod ea, delectus illum blanditiis eveniet fugiat alias assumenda voluptates dolore rem debitis. Sed voluptatum dignissimos culpa, praesentium natus consequatur? Perferendis adipisci aut consequatur beatae tempore quia dicta, id atque officiis illum!</p>
+           </div>
+       </div>
+   </div>
