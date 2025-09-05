f7ebaef00bb8ab1fa268f52eef13870e94e005e5
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-05 10:46:39 +0700

    model 2 selesai

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,4 @@ $routes->get('/', 'Pages::index');
 $routes->get('/about', 'Pages::about');
 $routes->get('/contact', 'Pages::contact');
 $routes->get('/komik', 'Komik::index');
+$routes->get('/komik/(:any)', 'Komik::detail/$1');
--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -1,22 +1,34 @@
 <?php
 
 namespace App\Controllers;
+
 use \App\Models\KomikModel;
 
 class Komik extends BaseController
-{   
+{
     protected $komikModel;
     public function __construct()
     {
         $this->komikModel = new KomikModel();
     }
+
     public function index(): string
-    {   
-        $komik = $this->komikModel->findAll();
+    {
+        $komik = $this->komikModel->getKomik();
         $data = [
             'title' => "Daftar Komik",
             'komik' => $komik
         ];
         return view('Komik/index', $data);
+    }
+
+    public function detail($slug): string
+    {
+        $komik = $this->komikModel->getKomik($slug);
+        $data = [
+            'title' => "Daftar Komik",
+            'komik' => $komik
+        ];
+        return view('Komik/detail', $data);
     }
 }
